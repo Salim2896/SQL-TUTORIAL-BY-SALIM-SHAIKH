61afb59bad3e1f247d68b8570eb08594ff703dd7
Salim Shaikh <99103971+Salim2896@users.noreply.github.com>
2024-12-21 05:17:21 -0500

    13. CTEs in MYSQL ADVANCED.sql

--- 13. CTEs in MYSQL ADVANCED.sql
+++ 13. CTEs in MYSQL ADVANCED.sql
@@ -1,7 +1,7 @@
 -- CTEs  (Common Table Expression)
 
 
-with CTE_EXAMPLE as 
+with CTE_EXAMPLE (Gender,Avg_Sal, Max_Sal, Min_Sal, Count_Sal) as 
 (
 select gender, avg(salary) as avg_sal, max(salary) as max_sal, Min(salary) as min_sal, count(salary) as count
 from parks_and_recreation.employee_demographics as dem
@@ -9,9 +9,9 @@ join parks_and_recreation.employee_salary as sal
 	on dem.employee_id = sal.employee_id
 group by gender 
 )
-
-select avg(avg_sal)
-from CTE_EXAMPLE;
+select *
+from CTE_EXAMPLE
+;
 
 
 -- Example how this is different than CTE and how CTE is more convenient
@@ -23,6 +23,30 @@ from CTE_EXAMPLE;
 -- 	on dem.employee_id = sal.employee_id
 -- group by gender 
 -- ) as example_subquery 
+
+
+with CTE_EXAMPLE as 
+(
+select employee_id, gender, birth_date
+from parks_and_recreation.employee_demographics 
+where birth_date > '1985-01-01' 
+),
+CTE_EXAMPLE2 as
+(
+select employee_id, salary
+from parks_and_recreation.employee_salary 
+where salary > 50000
+)
+select *
+from CTE_EXAMPLE
+join CTE_EXAMPLE2
+	on CTE_EXAMPLE.employee_id = CTE_EXAMPLE2.employee_id
+;
+
+
+
+
+
 
 
 
